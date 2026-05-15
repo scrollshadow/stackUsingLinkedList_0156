dcf8e18ef6c34d15c8f10195de637ef706926458
scrollshadow <zaldhypramana@gmail.com>
2026-05-15 14:26:34 +0700

    1

---
+++ stack.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        next = NULL;
+    }
+};
+
+class Stack
+{
+    private:
+    
+}
